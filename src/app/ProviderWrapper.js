c5a86d0547dec8b970f487fc2de695bfc3f6d066
Preethi Prabhakar <preethi@yamak.ai>
2023-02-15 13:25:46 +0530

    fixed page wrapper issue

--- src/app/ProviderWrapper.js
+++
@@ -1,8 +0,0 @@
-"use client";
-import "./globals.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { SessionProvider } from "next-auth/react";
-
-export default function ProviderWrapper({ children, ...props }) {
-  return <SessionProvider {...props}>{children}</SessionProvider>;
-}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,14 @@
 "use client";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ProviderWrapper from "./ProviderWrapper";
+import { SessionProvider } from "next-auth/react";
 
 export default function RootLayout({ children, session }) {
   return (
     <html lang="en">
       <head />
       <body>
-        <ProviderWrapper session={session}>{children}</ProviderWrapper>
+        <SessionProvider session={session}>{children}</SessionProvider>
       </body>
     </html>
   );
